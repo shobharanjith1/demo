12f47238ccc55b8067f706f6923801778326fe4b
Shobha P <sonuu.shobha@gmail.com>
2019-03-05 16:51:23 +0530

    Reading data from excel

--- firstt/src/firstt/readData.java
+++ firstt/src/firstt/readData.java
@@ -17,40 +17,7 @@ import org.testng.annotations.Test;
 
 public class readData {
 	
-	@Test
-	public void test1() throws EncryptedDocumentException, InvalidFormatException, IOException
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter subject");
-		String st = sc.next();
-		FileInputStream fis=new FileInputStream("./data/input.xlsx");
-		Workbook W = WorkbookFactory.create(fis);
-		Sheet s = W.getSheet("Sheet1");
-		Row r = s.getRow(0);
-		int c = r.getPhysicalNumberOfCells();
-		System.out.println(c);
-		//String s1="social";
-		int col=-1;
-		for(int i=0;i<c;i++)
-		{
-			String s2 = r.getCell(i).getStringCellValue();
-			boolean flag = s2.equals(st);
-			if(flag==true)
-			{
-				col = i;
-				break;
-			}
-		}
-		int r1 = s.getLastRowNum();
-		System.out.println(r1);
-		for(int j=0;j<r1;j++) {
-		String s3 = s.getRow(j).getCell(col).getStringCellValue();
-		System.out.println(s3);
-		}
-		
-		
-	}
-
+	
 	@Test
 	public void test() throws EncryptedDocumentException, InvalidFormatException, IOException
 	{
